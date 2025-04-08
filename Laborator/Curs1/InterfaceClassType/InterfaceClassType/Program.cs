e0fd2dc0977b51845af029a1a47d3a07ec34edef
Alin <alinalin03030@gmail.com>
2025-04-08 12:32:05 +0000

    Basic Classes exercise in C#

---
+++ Laborator/Curs1/InterfaceClassType/InterfaceClassType/Program.cs
@@ -0,0 +1,38 @@
+﻿
+using System;
+
+
+namespace InterfaceClassType;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        
+    }
+}
+
+public interface ICalculeaza
+{
+    decimal CalculeazaSalariu();
+}
+
+public class AngajamPartTime : ICalculeaza
+{
+    public string Nume { get; set; }
+    public decimal TarifPeOra { get; set; }
+    public int OreLucrate { get; set; }
+
+    public AngajamPartTime(string nume, decimal tarifPeOra, int oreLucrate)
+    {
+        Nume = nume;
+        TarifPeOra = tarifPeOra;
+        OreLucrate = oreLucrate;
+    }
+
+    public decimal CalculeazaSalariu()
+    {
+        return TarifPeOra * OreLucrate;
+    }
+    
+}
