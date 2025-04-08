485096bec5ffdbb133e86f2fa570bc85cfd6f55f
Alin <alinalin03030@gmail.com>
2025-04-08 17:51:28 +0000

    Function Exercize in c#

---
+++ Laborator/Curs1/newAplication/newAplication/Program.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        SenzorTemperatura senzor = new SenzorTemperatura();
+
+        senzor.AlarmaPornita += TemperaturaPericuloasa;
+
+        senzor.VerificaTemperatura(85);
+        senzor.VerificaTemperatura(102);
+    }
+
+    public static void TemperaturaPericuloasa(double temp)
+    {
+        Console.WriteLine($"Alera! Temperatura periculoasa {temp}");
+    }
+}
+
+public class SenzorTemperatura
+{
+    public delegate void AlarmHandler(double temperatura);
+    public event AlarmHandler AlarmaPornita;
+
+    public void VerificaTemperatura(double temperatura)
+    {
+        Console.WriteLine($"Temperatura citita :  {temperatura}");
+        if(temperatura > 100)
+            AlarmaPornita?.Invoke(temperatura);
+    }
+}
