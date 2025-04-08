7e6771673db9bf0c0931fcace34e4dcec6695c2a
Alin <alinalin03030@gmail.com>
2025-04-08 12:24:26 +0000

    Basic Classes exercise in C#

--- Laborator/Curs1/CSharpYoutubeTutorial/CSharpYoutubeTutorial/Program.cs
+++ Laborator/Curs1/CSharpYoutubeTutorial/CSharpYoutubeTutorial/Program.cs
@@ -10,11 +10,81 @@ class Program
 {
     static void Main(string[] args)
     {
-        
+        Vehicul masina = new Masina(1200, "Masina", "Autovehicul", 4, "4WeelDrive", 1040, 5);
+        masina.Porneste();
     }
 }
 
-class Animal
+class Vehicul
+{
+    private int greutate;
+    private string clasa;
+    private string tipTransport;
+
+    public Vehicul(int g, string cla, string tip)
+    {
+        greutate = g;
+        clasa = cla;
+        tipTransport = tip;
+    }
+
+    public virtual void Porneste()
+    {
+        Console.WriteLine("Porneste Vehiculul");
+        Console.WriteLine("Greutate " + greutate);
+        Console.WriteLine("Clasa " + clasa);
+        Console.WriteLine("Tip Transport " + tipTransport);
+    }
+}
+
+class Masina : Vehicul
+{
+    private int tractiune;
+    private string tractiuneTip;
+    private int caiPutere;
+    private int nrUsi;
+
+    public Masina(int g, string cla, string tip, int tractiune, string tractiuneTip, int caiPutere, int nrUsi) : base(g, cla, tip)
+    {
+        this.tractiune = tractiune;
+        this.tractiuneTip = tractiuneTip;
+        this.caiPutere = caiPutere;
+        this.nrUsi = nrUsi;
+    }
+
+    public override void Porneste()
+    {
+        Console.WriteLine("Porneste Masina");
+        Console.WriteLine("Tractiune " + tractiune);
+        Console.WriteLine("Tractiune " + tractiuneTip);
+        Console.WriteLine("Cai Putere " + caiPutere);
+        Console.WriteLine("Nr Usi " + nrUsi);
+    }
+}
+
+class MasinaElectrica : Masina
+{
+    private int mah;
+    private int vitezaDeIncarcare;
+    private string culoare;
+
+    public MasinaElectrica(int g, string cla, string tip, int tractiune, string tractiuneTip, int caiPutere, int nrUsi, int mah, int vitezaDeIncarcare, string culoare) : base(g, cla, tip, tractiune, tractiuneTip, caiPutere, nrUsi)
+    {
+        this.mah = mah;
+        this.vitezaDeIncarcare = vitezaDeIncarcare;
+        this.culoare = culoare;
+    }
+
+    public override void Porneste()
+    {
+        Console.WriteLine("Porneste Masina Electrica");
+        Console.WriteLine("Mah " + mah);
+        Console.WriteLine("Viteza de inccarcare : " + vitezaDeIncarcare);
+        Console.WriteLine("Coloare : " + culoare);
+    }
+}
+
+/*class Animal
 {
     private string nume;
     private string specie;
@@ -42,4 +112,5 @@ class Pisica : Animal
         varsta = v;
         nrMembre = nr;
     }
-}
+    
+}*/
--- Laborator/Curs1/CSharpYoutubeTutorial/CSharpYoutubeTutorial/obj/Debug/net9.0/CSharpYoutubeTutorial.AssemblyInfo.cs
+++ Laborator/Curs1/CSharpYoutubeTutorial/CSharpYoutubeTutorial/obj/Debug/net9.0/CSharpYoutubeTutorial.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CSharpYoutubeTutorial")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fc7de167984d0a894a14790f4c377b90833daf4")]
 [assembly: System.Reflection.AssemblyProductAttribute("CSharpYoutubeTutorial")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CSharpYoutubeTutorial")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
