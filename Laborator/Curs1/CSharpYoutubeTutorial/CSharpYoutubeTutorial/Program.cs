2fc7de167984d0a894a14790f4c377b90833daf4
Alin <alinalin03030@gmail.com>
2025-04-08 11:11:47 +0000

    Classes in C#

--- Laborator/Curs1/CSharpYoutubeTutorial/CSharpYoutubeTutorial/Program.cs
+++ Laborator/Curs1/CSharpYoutubeTutorial/CSharpYoutubeTutorial/Program.cs
@@ -13,3 +13,33 @@ class Program
         
     }
 }
+
+class Animal
+{
+    private string nume;
+    private string specie;
+    private int greutate;
+
+    public Animal(string n, string s, int g)
+    {
+        nume = n;
+        specie = s;
+        greutate = g;
+    }
+    
+    public void Sunet()
+    {
+        Console.WriteLine(nume);
+    }
+}
+
+class Pisica : Animal
+{
+    private int varsta;
+    private int nrMembre;
+    public Pisica(string n, string s, int g, int v, int nr) : base(n, s, g)
+    {
+        varsta = v;
+        nrMembre = nr;
+    }
+}
