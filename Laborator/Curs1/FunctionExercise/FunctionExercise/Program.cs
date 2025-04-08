f04b95235a907b98bc1276ad7bdcf824c50af58c
Alin <alinalin03030@gmail.com>
2025-04-08 17:10:17 +0000

    Basic Classes exercise in C#

---
+++ Laborator/Curs1/FunctionExercise/FunctionExercise/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+public delegate bool ValidareNumar(int numar);
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        
+    }
+
+    public static List<int> FiltreazaNumere(List<int> numere, ValidareNumar validare)
+    {
+        List<int> rezultat = new List<int>();
+        foreach (int numar in numere)
+        {
+            if (validare(numar))
+            {
+                rezultat.Add(numar);
+            }
+        }
+        return rezultat;    
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
